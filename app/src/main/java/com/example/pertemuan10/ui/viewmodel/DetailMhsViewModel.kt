3b80d1fdf81c7de9e26339cde959ee60903137dd
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-15 22:37:29 +0700

    memindahkan data dari entitiy ke ui

---
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.pertemuan10.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_meet10.ui.viewmodel.HomeUiState
+import com.example.pertemuan10.data.entity.Mahasiswa
+import com.example.pertemuan10.repository.RepositoryMhs
+import com.example.pertemuan10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+//data class untuk menampung data yang akan ditampilkan di ui
+
+//memindahkann data dari entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
+
