1aba28badf29185d4a39c94e50f3735bae83aa6b
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-15 22:29:10 +0700

    membuat data class HomeUiState di view model

---
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.pam_meet10.ui.viewmodel
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+import com.example.pertemuan10.data.entity.Mahasiswa
+import com.example.pertemuan10.repository.RepositoryMhs
+import com.example.pertemuan10.ui.costumwidget.appBar
+import com.example.pertemuan10.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+
+
+// state; mengubah tampilan
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
