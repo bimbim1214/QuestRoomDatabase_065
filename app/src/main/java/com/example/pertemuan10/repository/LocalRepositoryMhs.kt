9a0819d649f367bfb538aca0f0b5711a06d6c3b9
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-15 22:15:27 +0700

    function untuk memanggil fungsi getAllMahsiswa dari MahasiswaDao untuk mendapatkan data mahasiswa dalam bentuk data flow

--- app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
@@ -2,12 +2,18 @@ package com.example.pertemuan10.repository
 
 import com.example.pertemuan10.data.dao.MahasiswaDao
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+import java.lang.invoke.MethodHandle
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs{
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+    //getAllMhs
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
     }
 
 }
